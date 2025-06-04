f2346ea924c6620c3bb966f496c22665d709f4b8
SkyNet <arierhiprecious@gmail.com>
2025-06-04 11:02:49 +0100

    latest

--- vite.config.js
+++ vite.config.js
@@ -22,17 +22,17 @@ export default defineConfig({
         background_color: '#ffffff',
         icons: [
           {
-            src: 'icons/icon-192x192.png',
+            src: 'icons/192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'icons/icon-512x512.png',
+            src: 'icons/512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: 'icons/icon-512x512.png',
+            src: 'icons/512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
